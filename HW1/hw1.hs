6261081ccbc5e636cbd559104be4e809095b08dc
BJarv <brandon.jarv@gmail.com>
2015-01-14 04:00:47 -0800

    All but last question HW1

--- HW1/hw1.hs
+++ HW1/hw1.hs
@@ -21,9 +21,31 @@ bibliography_rec :: [(String, String, Int)] -> String
 bibliography_rec [] = ""
 bibliography_rec (x:xs) = citeBook x ++ "\n" ++ bibliography_rec xs
 
---7 bibliography_foldl
+--7. bibliography_foldl same as bibliography_rec
 bibliography_foldl :: [(String, String, Int)] -> String
 bibliography_foldl xs = foldl (\bib x -> bib ++ citeBook x ++ "\n") "" xs
 
+--8. averageYear [("", "", 1), ("", "", 3)] -- -> 2
+averageYear :: [(String, String, Int)] -> Int
+averageYear x = sum (years x) `div` length x
 
+--8a. years [("", "", 1), ("", "", 3)] -- -> [1, 3] 
+years :: [(String, String, Int)] -> [Int]
+years ys = [ y | (_, _, y) <- ys ]
 
+--9. references
+references :: String -> Int
+references n = length (filter (=='[') n)
+
+--10. citeText
+citeText :: [(String, String, Int)] -> String -> String
+citeText c b = filter' (== "[" ++ _ ++ "]") (words b) c
+
+filter' :: (a -> Bool) -> [String] -> [(String, String, Int)] -> [String]  
+filter' _ [] _ = []  
+filter' p (x:xs) ys
+    | p x       = b : filter' p xs ys -- prepend b
+    | otherwise = x : filter' p xs ys -- prepend whatever failed the filter
+    where b = citeBook (ys !! ((read (take 1 (drop 1 x)) :: Int) - 1)) 
+    -- take the number from inside the citation, read it to an int, subtract one for list vertices, and rather than add the citation bracket, add the citation selected by vertex
+    -- doesn't compile
